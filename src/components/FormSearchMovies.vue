d6ade3ce6a40e96493e6438ffa45c546f344497c
WilfriedTsague <2293518@cegeplimoilou.ca>
2024-05-06 19:05:59 +0100

    save 2

---
+++ src/components/FormSearchMovies.vue
@@ -0,0 +1,6 @@
+<template>
+     <form class="d-flex" role="search">
+            <input class="form-control me-2" placeholder="Recherche" aria-label="Recherche">
+            <button class="btn btn-outline-success" type="submit">Recherche</button>
+          </form>
+  </template>
